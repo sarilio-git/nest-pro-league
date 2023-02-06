b8fca0f27b721953e7532736f4d8cda0185ec13f
Diogo Sarilio <111839159+sarilio-git@users.noreply.github.com>
2023-02-06 12:40:52 -0500

    NPL

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,11 @@ export default function Document() {
   return (
     <Html data-theme="dark">
       <Head>
-
+      <Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-P658TWJ');`}}></Script>
         <meta
           name="description"
           content="Página para assistir o HUB da NPL"
@@ -37,12 +41,7 @@ export default function Document() {
         />
       </Head>
       <body>
-      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P658TWJ');</script>
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P658TWJ"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P658TWJ"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 
         <Main />
